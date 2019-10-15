17346446b40fae9913afc622d65ecb4434597652
Mihail87 <leontemihai@gmail.com>
2019-10-15 10:49:08 +0300

    Project setup

---
+++ ShootingGallery/GameScene.swift
@@ -0,0 +1,26 @@
+//
+//  GameScene.swift
+//  ShootingGallery
+//
+//  Created by Mihai Leonte on 10/15/19.
+//  Copyright © 2019 Mihai Leonte. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
